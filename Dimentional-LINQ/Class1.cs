bf385a0327453f636712fbf2d4dd8d478f1a2e42
PatAPizza56 <syncedstudiosdev@gmail.com>
2021-12-04 02:18:39 -0500

    Started
    Added first extension method, as well as example project and test.

--- Dimentional-LINQ/Class1.cs
+++ Dimentional-LINQ/Class1.cs
@@ -1,7 +1,22 @@
 ﻿namespace Dimentional_LINQ
 {
-    public class Class1
+    public static class Class1
     {
+        public static int[,] Where(this int[,] e, Func<int, int, bool> c)
+        {
+            int[,] r = e;
+            for (int i = 0; i < e.GetLength(0); i++)
+            {
+                for (int j = 0; j < e.GetLength(1); j++)
+                {
+                    if (!c(i, j))
+                    {
+                        r[i, j] = 0;
+                    }
+                }
+            }
 
+            return r;
+        }
     }
 }
---
+++ Examples/Program.cs
@@ -0,0 +1,30 @@
+﻿using Dimentional_LINQ;
+
+int[,] test = new int[5, 5];
+int num = 1;
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        test[i, j] = num;
+        num++;
+    }
+}
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.WriteLine(test[i, j]);
+    }
+}
+
+test = test.Where((i, j) => test[i, j] % 2 == 0);
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.WriteLine(test[i, j]);
+    }
+}
